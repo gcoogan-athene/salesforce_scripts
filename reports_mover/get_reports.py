7000449499f475a559d811635b6d6504f71209cb
Greg Coogan <gcoogan@athene.com>
2020-12-09 09:53:32 -0600

    Update to Python 3

--- reports_mover/get_reports.py
+++ reports_mover/get_reports.py
@@ -24,15 +24,15 @@ def dump_reports(soql):
 	""" Dumps Salesforce report details given a SOQL """
 	sf = sfdc.SFDC('sfdc.yaml')
 	if not sf.connected:
-		print "[ERROR]: No connection to Salesforce"
+		print ("[ERROR]: No connection to Salesforce")
 		return False
 
 	results = sf.run_soql(soql)
 	if not results or not results.get('totalSize', 0):
-		print "[INFO]: No reports matching search criterion"
+		print ("[INFO]: No reports matching search criterion")
 		return False
 
-	print "[INFO]: %d reports matched" % results.get('totalSize')
+	print ("[INFO]: %d reports matched" % results.get('totalSize'))
 	report_ids = []
 	lines = []
 	for record in results.get('records'):
@@ -50,15 +50,15 @@ def dump_reports(soql):
 	return True
 
 def main():
-	criteria = int(raw_input(MAIN_MENU_TEXT))
+	criteria = int(input(MAIN_MENU_TEXT))
 	if criteria == SEARCH_BY_FOLDER:
-		folder_name = raw_input("Enter folder name: ")
-		print "[INFO]: Searching for reports in folder %s" % folder_name
+		folder_name = input("Enter folder name: ")
+		print ("[INFO]: Searching for reports in folder %s" % folder_name)
 		soql = ("SELECT Id, Name, FolderName, LastRunDate FROM Report "
 				"WHERE IsDeleted = FALSE AND "
 				"FolderName = '%s'" % folder_name)
 	elif criteria == SEARCH_BY_NOT_RUN:
-		ndays = int(raw_input("Enter number of days: "))
+		ndays = int(input("Enter number of days: "))
 		print ("[INFO]: Searching for reports that were NOT "
 			   "executed in the last %d days" % ndays)
 		# if report was never executed, the LastRunDate will be null
@@ -67,14 +67,14 @@ def main():
 				"((LastRunDate = null) OR "
 				"(LastRunDate < LAST_N_DAYS:%d))" % ndays)
 	elif criteria == SEARCH_BY_RUN:
-		ndays = int(raw_input("Enter number of days: "))
+		ndays = int(input("Enter number of days: "))
 		print ("[INFO]: Searching for reports that were "
 			   "executed in the last %d days" % ndays)
 		soql = ("SELECT Id, Name, FolderName, LastRunDate FROM Report "
 				"WHERE IsDeleted = FALSE AND "
 				"LastRunDate >= LAST_N_DAYS:%d" % ndays)
 	else:
-		print "Invalid search option."
+		print ("Invalid search option.")
 		return
 
 	dump_reports(soql)
